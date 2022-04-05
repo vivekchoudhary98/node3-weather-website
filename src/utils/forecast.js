fbff8cb965b6404ea2a92c13a82b913eed74f0b8
vivek choudhary <vivek301298@gmail.com>
2022-04-05 12:05:51 +0530

    add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,14 +1,20 @@
 const request=require('request')
 const forecast = (latitude,longitude,callback)  => {
     const url='http://api.weatherstack.com/current?access_key=9932c9041d7850168c85743616a4691f&query='+ latitude + ',' + longitude +'&units=f'
-request({url,json: true},(error, {body})=> {
+    request({url,json: true},(error, {body})=> {
         if (error) {
             callback('unable toconnect to service',undefined)
         }
         else if(body.error){
             callback('unable tofiind location',undefined)   
         }else {
-            callback(undefined,'it is currently' +  body.current.temperature + 'degrees out it eels like' + body.current.feelslike)
+            console.log()
+            callback(undefined,
+                body.current.weather_descriptions[0] + 
+                ' it is currently ' +  body.current.temperature 
+                + ' degrees out. it feels like ' + 
+                body.current.feelslike + ' degress.out. the humidity is '
+                +  body.current.humidity +' %')
         }
     })
 
